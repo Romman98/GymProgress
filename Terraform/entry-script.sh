3fb79ea037e8c35ed963decd36a7533a44406c06
ahmad romman <romman.ahmad@outlook.com>
2025-09-01 19:49:47 +0300

    Editing the entry-script.sh

--- Terraform/entry-script.sh
+++ Terraform/entry-script.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 # Update packages
 sudo dnf update -y
-
+echo "done" > log.txt
 # Install Docker
 sudo dnf install -y docker
-
-# Start Docker and enable at boot
 sudo systemctl start docker
 sudo systemctl enable docker
-
-# Add ec2-user to Docker group
 sudo usermod -aG docker ec2-user
-
+echo "done" >> log.txt
 # Format the volume if not already
 sudo mkfs -t ext4 /dev/nvme1n1 || true
 
@@ -23,15 +19,17 @@ sudo mount /dev/nvme1n1 /mnt/gymprogress_data
 
 # Add to fstab to mount on reboot
 echo '/dev/nvme1n1 /mnt/gymprogress_data ext4 defaults,nofail 0 2' | sudo tee -a /etc/fstab
+echo "done" >> log.txt
 
 # Ensure docker data directory exists on volume
 sudo mkdir -p /mnt/gymprogress_data/docker
 sudo chown -R ec2-user:ec2-user /mnt/gymprogress_data/docker
+echo "done" >> log.txt
 
 
 # Pull and run your container
 docker pull romman1998/gymprogress:latest
-cat <<EOF > /home/ec2-user/deploy.sh
+cat << 'EOF' > /home/ec2-user/deploy.sh
 #!/bin/bash
 
 CONTAINERS=$(docker ps -aq)
@@ -40,9 +38,7 @@ TAG="latest"
 PORT=80
 
 runContainer() {
-        docker run -d -p $PORT:80 --name gymprocess \
-        -v /mnt/gymprogress_data/docker:/app/instance
-                $IMAGE_NAME:$TAG
+        docker run -d -p $PORT:80 --name gymprocess -v /mnt/gymprogress_data/docker:/app/instance $IMAGE_NAME:$TAG
 }
 
 docker pull "$IMAGE_NAME":"$TAG"
@@ -51,10 +47,12 @@ if [[ -n "$CONTAINERS" ]];then
         docker stop "$CONTAINERS"
         docker rm "$CONTAINERS"
         runContainer
-
+        echo "done2" >> log.txt
 else
         runContainer
-fi 
+        echo "done3" >> log.txt
+
+fi
 EOF
 
 chmod +x /home/ec2-user/deploy.sh
