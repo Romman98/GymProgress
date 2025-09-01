9a2f490332437fa1d76575f483b25ab01be77780
Ahmad Romman <141449003+Romman98@users.noreply.github.com>
2025-09-01 12:08:46 +0300

    Update entry-script.sh

--- Terraform/entry-script.sh
+++ Terraform/entry-script.sh
@@ -14,4 +14,29 @@ sudo usermod -aG docker ec2-user
 
 # Pull and run your container
 docker pull romman1998/gymprogress:latest
-docker run -d -p 80:80 --name gymprogress romman1998/gymprogress:latest
+cat <<EOF >> /home/ec2-user/deploy.sh
+#!/bin/bash
+
+CONTAINERS=$(docker ps -aq)
+IMAGE_NAME="romman1998/gymprogress"
+TAG="latest"
+PORT=80
+
+runContainer() {
+        docker run -d -p $PORT:80 --name gymprocess \
+                $IMAGE_NAME:$TAG
+}
+
+if [[ -n "$CONTAINERS" ]];then
+        docker stop "$CONTAINERS"
+        docker rm "$CONTAINERS"
+        runContainer
+
+else
+        runContainer
+fi 
+EOF
+
+chmod +x /home/ec2-user/deploy.sh
+/home/ec2-user/deploy.sh
+
